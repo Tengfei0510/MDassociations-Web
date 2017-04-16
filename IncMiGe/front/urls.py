0be7086bd781faba772af91a93f27130646eb1b1
kevin <tutengfei.kevin@gmail.com>
2017-04-16 11:20:21 +0800

    add conact page

--- IncMiGe/front/urls.py
+++ IncMiGe/front/urls.py
@@ -7,8 +7,9 @@ from django.conf.urls import url
 
 from . import views
 
-app_name = 'web'
+app_name = 'front'
 
 urlpatterns = [
     url(r'^$', views.index_view, name='index'),
+    url(r'^contact$', views.contact_view, name='contact'),
 ]
--- IncMiGe/front/views.py
+++ IncMiGe/front/views.py
@@ -4,3 +4,7 @@ from django.shortcuts import render
 # Create your views here.
 def index_view(request):
     return render(request, 'front/index.html')
+
+
+def contact_view(request):
+    return render(request, 'front/contact.html')
